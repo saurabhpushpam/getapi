6718ac524596dc1aa9e5d542f2b0197daf32544b
saurabhpushpam <145956198+saurabhpushpam@users.noreply.github.com>
2023-10-17 13:17:25 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const config= {
-    secret_jwt: "thisismysecretkey",
-    emailUser: 'spuspam111@gmail.com',
-    emailPassword: 'nxrh cjsb stsu hrar'
+    secret_jwt: "thisismysecretkey"
+   
 
 }
 
